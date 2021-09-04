1c5fa1b67bfbb24a82c3f6e94913b83b94344266
Iran Portela <iranportela@gmail.com>
2021-09-04 19:46:35 -0300

    Colocação do Scanner

--- src/apllication/Programa.java
+++ src/apllication/Programa.java
@@ -1,10 +1,16 @@
 package apllication;
 
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String nome = sc.nextLine();
+		System.out.println("Nome: " + nome);
+		
+		sc.close();
 	}
 
 }
